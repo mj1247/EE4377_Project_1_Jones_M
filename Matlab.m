7452b560ad28ad49f036a673ef84ea59dcb83aba
mj1247 <mj1247@txstate.edu>
2015-02-27 19:22:20 -0600

    changed author name changed function name changed date added 4 subplots and one extra function

--- Matlab.m
+++ Matlab.m
@@ -1,7 +1,7 @@
-% EExxxx Project 1
-% MATLAB code for xxxxxxxxxxxxx
-% Your Name and Last Name
-% Date
+% EE4377 Project 1
+% MATLAB code for Matlab
+%Monique Jones
+% 2/27/15
 
 clc      % Clear screen 
 clear    % Clear all variables
@@ -9,10 +9,13 @@ A = 3; f= 1000; omega = 2*pi*f;
 angle = degtorad(33); T=1/f;
 t=linspace(-3*T,3*T,1000);
 x_t=3*cos(omega*t+angle);
-subplot(3,1,1)
+f_t=cos(omega*t)*sin(omega*t)*u(t-1);
+subplot(4,1,1)
 plot(t,x_t, 'linewidth',2);grid on
-subplot(3,1,2)
+subplot(4,1,2)
 u_t=heaviside(t);
 plot(t,u_t, 'linewidth',2);grid on
-subplot(3,1,3)
+subplot(4,1,3)
 plot(t,x_t.*u_t, 'linewidth',2);grid on
+subplot(4,1,4)
+plot(t,f_t,'linewidth',2);grid on
